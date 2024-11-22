b4e04abdf6c86713098d4371833503cb635d490d
cen69005 <abelikova@csas.cz>
2024-11-22 20:52:05 +0100

    initial commit

---
+++ src/main/java/com/example/shipmonk/ShipmonkApplication.java
@@ -0,0 +1,13 @@
+package com.example.shipmonk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShipmonkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShipmonkApplication.class, args);
+    }
+
+}
